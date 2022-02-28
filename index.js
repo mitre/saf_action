d957dfb0ddfbdc6ec9d9573e9c0de6177be2b154
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-28 10:01:15 -0600

    add check for allowable commands
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ if(!command_string) {
     throw new Error("SAF CLI Command String argument is required.");
 }
 
+const allowable_commands = ['convert', 'generate', 'harden', 'scan', 'validate', 'view'];
+if(!allowable_commands.includes(command_string.split(':')[0])) {
+    throw new Error("The command string did not include one of the allowable commands: " + allowable_commands.join(', ') + ". Please reference the documentation for more details.");
+}
+
 if(command_string.split(" ")[0] == "view:heimdall") {
     throw new Error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.");
 }
