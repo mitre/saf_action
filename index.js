60cb65c60bbf9defe43839d130f27a81edad543c
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-03-30 08:30:31 -0500

    fix topic and command
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ if(!command_string) {
 const saf_command = command_string.split(' ');
 
 const allowable_topics = ['convert', 'generate', 'harden', 'scan', 'validate', 'view'];
-const topic = saf_command[0].split(':')[0] || saf_command[0];
+const topic = saf_command[0].split(':')[0];
 
 if(!allowable_topics.includes(topic)) {
     throw new Error("The command string did not include one of the allowable topics: " + allowable_topics.join(', ') + ". Please reference the documentation for more details.");
 }
 
-const command = saf_command[0].split(':')[1] || saf_command[1];
+const command = saf_command[0].split(':')[1];
 
 if(topic == "view" & command == "heimdall") {
     throw new Error("The SAF Action does not support the 'view heimdall' command. Please reference the documentation for other uses.");
