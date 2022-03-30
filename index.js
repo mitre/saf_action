1640ce9dd9746db4eb83a4590c2a4f09306799d8
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-03-30 15:34:43 -0500

    test by awaiting saf_action
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const core = require('@actions/core');
-const saf = require('@mitre/saf');
+const saf_action =  require('run_command.js');
 
-const command_string = core.getInput('command_string');
-if(!command_string) {
-    throw new Error("SAF CLI Command String argument is required.");
-}
-
-const saf_command = command_string.split(' ');
-
-const allowable_topics = ['convert', 'generate', 'harden', 'scan', 'validate', 'view'];
-const topic = saf_command[0].split(':')[0];
-
-if(!allowable_topics.includes(topic)) {
-    throw new Error("The command string did not include one of the allowable topics: " + allowable_topics.join(', ') + ". Please reference the documentation for more details.");
-}
-
-const command = saf_command[0].split(':')[1];
-
-if(topic == "view" & command == "heimdall") {
-    throw new Error("The SAF Action does not support the 'view heimdall' command. Please reference the documentation for other uses.");
-}
-
-saf.run(saf_command);
+saf_action();
--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,17 @@
 const path = require('path');
+const saf_action =  require('../run_command.js');
 
 describe('SAF Action tests', () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
   it('should run SAF with a space topic separator in the command string', async () => {
     let inputFilePath = path.resolve('./test/sample_data/red_hat_good.json');
     process.env['INPUT_COMMAND_STRING'] = `view summary -i ${inputFilePath}`;
-    require('../index.js');
-    await new Promise((r) => setTimeout(r, 1000));
+    await saf_action();
     delete process.env['INPUT_COMMAND_STRING'];
   });
-  it('should run SAF with a semicolon topic separator in the command string', async () => {
+  it('should run SAF with a colon topic separator in the command string', async () => {
     let inputFilePath = path.resolve('./test/sample_data/red_hat_good.json');
     process.env['INPUT_COMMAND_STRING'] = `view:summary -i ${inputFilePath}`;
-    require('../index.js');
-    await new Promise((r) => setTimeout(r, 1000));
+    await saf_action();
     delete process.env['INPUT_COMMAND_STRING'];
   });
 });
