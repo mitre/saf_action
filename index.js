6285e81d35404a8e3831e9a98933caa99341e7c9
Emily Rodriguez <32680215+em-c-rod@users.noreply.github.com>
2023-04-17 09:58:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const saf_action =  require('./run_command.js');
+const path = require('node:path');
 
-saf_action({safCLIPath: "/home/runner/work/_actions/mitre/saf_action/main/node_modules/@mitre/saf/lib/index.js"});
+saf_action({safCLIPath: path.join(process.env.HOME, 'work/_actions/mitre/saf_action/main/node_modules/@mitre/saf/lib/index.js')});
