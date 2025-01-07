b91d5f20d9f0d6f19d4cf17df2eb346f0cfbdfa3
Amndeep Singh Mann <amann@mitre.org>
2025-01-07 15:14:47 -0500

    fix relative path
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ console.log(process.env.GITHUB_ACTION_REF);
 console.log(process.env.GITHUB_ACTION_PATH); // apparently we're not a composite action
 console.log(process.env.GITHUB_WORKSPACE);
 // console.log(path.join(process.env.GITHUB_WORKSPACE, '..', 'node_modules/@mitre/saf/lib/index.js'));
-console.log(path.join(process.env.GITHUB_WORKSPACE, '../..', 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
+console.log(path.join(process.env.GITHUB_WORKSPACE, '..', 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
 console.log(path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
-saf_action({safCLIPath: path.join(process.env.GITHUB_WORKSPACE, '../..', 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js')});
+saf_action({safCLIPath: path.join(process.env.GITHUB_WORKSPACE, '..', 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js')});
