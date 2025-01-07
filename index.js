7d89e2494c6746876c0286e47190dd64e5ce93cf
Amndeep Singh Mann <amann@mitre.org>
2025-01-07 15:23:03 -0500

    get rid of envvar that fails on window every time and also print out tree
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ console.log(process.env.GITHUB_ACTION_PATH); // apparently we're not a composite
 console.log(process.env.GITHUB_WORKSPACE);
 // console.log(path.join(process.env.GITHUB_WORKSPACE, '..', 'node_modules/@mitre/saf/lib/index.js'));
 console.log(path.join(process.env.GITHUB_WORKSPACE, '../..', '_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
-console.log(path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
+
+const execSync = require('child_process').execSync;
+const output = execSync(`tree ${path.join(process.env.GITHUB_WORKSPACE, '../..', '_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js')}`, { encoding: 'utf-8' });
+console.log(output);
+
+// console.log(path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
 saf_action({safCLIPath: path.join(process.env.GITHUB_WORKSPACE, '../..', '_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js')});
