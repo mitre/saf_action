047891fe2778f38b46bde0c2e0b3205db361c060
Amndeep Singh Mann <amann@mitre.org>
2025-01-07 13:15:11 -0500

    debugging
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const saf_action =  require('./run_command.js');
 const path = require('node:path');
 
+console.log("what is this path");
+console.log(path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
 saf_action({safCLIPath: path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js')});
