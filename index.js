2e729aad90d9c7cb96b9c9d05ce32794ade67028
Amndeep Singh Mann <amann@mitre.org>
2025-01-07 13:44:14 -0500

    confirmed that process.env.HOME does not exist on windows side, found a new github envvar that might do what we need it to do
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ console.log(process.env.HOME);
 console.log(process.env.GITHUB_ACTION_REPOSITORY);
 console.log(process.env.GITHUB_ACTION_REF);
 console.log(path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
+console.log(path.join(process.env.GITHUB_ACTION_PATH, 'node_modules/@mitre/saf/lib/index.js'));
 saf_action({safCLIPath: path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js')});
