09e66a87b0508f97575e33a3d993a2a73f9f9c6b
Amndeep Singh Mann <amann@mitre.org>
2025-01-07 15:03:24 -0500

    comment syntax lol
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("what is this path");
 console.log(process.env.HOME);
 console.log(process.env.GITHUB_ACTION_REPOSITORY);
 console.log(process.env.GITHUB_ACTION_REF);
-console.log(process.env.GITHUB_ACTION_PATH); # apparently we're not a composite action
+console.log(process.env.GITHUB_ACTION_PATH); // apparently we're not a composite action
 console.log(process.env.GITHUB_WORKSPACE);
 console.log(path.join(process.env.GITHUB_WORKSPACE, '..', 'node_modules/@mitre/saf/lib/index.js'));
 console.log(path.join(process.env.HOME, 'work/_actions/', process.env.GITHUB_ACTION_REPOSITORY, process.env.GITHUB_ACTION_REF, 'node_modules/@mitre/saf/lib/index.js'));
